61125a3a58bcb7f596dd7dbf1e2bcb1cd55d2c63
Louis Tran <louist@zillow.com>
2016-02-23 04:14:03 -0800

    Initial Commit

---
+++ overlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  overlay
+//
+//  Created by Louis Tran on 2/23/16.
+//  Copyright © 2016 IF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ overlayTests/overlayTests.m
@@ -0,0 +1,39 @@
+//
+//  overlayTests.m
+//  overlayTests
+//
+//  Created by Louis Tran on 2/23/16.
+//  Copyright © 2016 IF. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface overlayTests : XCTestCase
+
+@end
+
+@implementation overlayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
